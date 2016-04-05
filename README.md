73e4f6d0e66bf944d60c7bb51d95ff9b5782beb8
Rene <rene.mejiac@gmail.com>
2016-04-05 18:23:45 +0000

    + Initial commit - project is up to list users and add users view

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# taskmanager
-Simple task manager using Symfony 2.7
+taskmanager
+===========
+
+A Symfony project created on March 31, 2016, 3:55 pm.
---
+++ src/EMM/UserBundle/Controller/UserController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace EMM\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use EMM\UserBundle\Entity\User;
+use EMM\UserBundle\Form\UserType;
+
+class UserController extends Controller
+{
+    public function indexAction(){
+        
+        
+        
+        $em = $this->getDoctrine()->getManager();
+        $users = $em->getRepository('EMMUserBundle:User')->findAll();
+        // $res = 'Lista de usuarios: <br/>';
+        // foreach($users as $user){
+        //     $res .= 'Usuario: ' . $user->getUsername() . ' - Email: ' . $user->getEmail() . '<br/>';
+        // }
+        
+        // return new Response($res);
+        
+        return $this->render('EMMUserBundle:User:index.html.twig', array('users' => $users));
+    }
+    
+    public function viewAction($id){
+        
+        $repository = $this->getDoctrine()->getRepository('EMMUserBundle:User');
+        //$user = $repository->find($id);
+        //$user = $repository->findOneByUsername($nombre);
+        $user = $repository->findOneById($id);
+        return new Response('Usuario: ' . $user->getUsername() . ' con email: ' . $user->getEmail() );
+        
+    }
+    
+    public function articlesAction($page)
+    {
+        return new Response('Este es mi artículo ' . $page);
+    }
+    
+    public function addAction(){
+        $user = new User();
+        $form = $this->createCreateForm($user);
+        
+        return $this->render('EMMUserBundle:User:add.html.twig', array('form' => $form->createView()));
+        
+    }
+    
+    private function createCreateForm(User $entity){
+        $form = $this->createForm(new UserType(), $entity, array(
+                'action' => $this->generateUrl('emm_user_create'),
+                'method' => 'POST'
+            ));
+            
+        return $form;
+    }
+    
+    public function createAction(Request $request){
+        $user =  new User();
+        $form = $this->createCreateForm($user);
+        $form->handleRequest($request);
+        
+        if($form->isValid()){
+            
+            $password = $form->get('password')->getData();
+            
+            $encoder = $this->container->get('security.password_encoder');
+            $encoded = $encoder->encodePassword($user, $password);
+            
+            $user->setPassword($encoded);
+            
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            
+            $em->flush();
+            
+            return $this->redirectToRoute('emm_user_index');
+        }
+        
+        return $this->render('EMMUserBundle:User:add:html:twig', array('form' => $form->createView()));
+    }
+    
+    
+}
